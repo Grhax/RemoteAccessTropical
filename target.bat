1875033cff5db71075c12f8d39cbb9a254ff9a92
Grhax <117516146+Grhax@users.noreply.github.com>
2025-07-27 22:55:48 +0300

    Update target.bat

--- target.bat
+++ target.bat
@@ -1,18 +1,1 @@
-@echo off
-title Process Hacker Tespit
-color 4F
-setlocal enabledelayedexpansion
-:loop
-tasklist /FI "IMAGENAME eq ProcessHacker.exe" | find /I "ProcessHacker.exe" >nul
-if %ERRORLEVEL%==0 (
-    color 2F
-    cls
-    echo [✓] Process Hacker bulundu!
-) else (
-    color 4F
-    cls
-    echo [X] Process Hacker bulunamadi!
-)
 
-timeout /t 1 >nul
-goto loop
